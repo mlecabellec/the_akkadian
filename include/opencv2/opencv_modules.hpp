37a749a7b68ebc149de8299b440eada26bb35c1c
mlecabellec <mickael.lecabellec@gmail.com>
2020-12-27 23:39:05 +0100

    Integrated opencv WiringPi cpp-propertes picotts and tesseract.

--- include/opencv2/opencv_modules.hpp
+++ include/opencv2/opencv_modules.hpp
@@ -1,17 +1,8 @@
 // Then the optional modules are checked
-#ifdef HAVE_OPENCV_CALIB3D
-#include "opencv2/calib3d.hpp"
-#endif
-#ifdef HAVE_OPENCV_FEATURES2D
-#include "opencv2/features2d.hpp"
-#endif
-#ifdef HAVE_OPENCV_DNN
-#include "opencv2/dnn.hpp"
-#endif
-#ifdef HAVE_OPENCV_FLANN
-#include "opencv2/flann.hpp"
-#endif
-
+#define HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_FEATURES2D
+#define HAVE_OPENCV_DNN
+#define HAVE_OPENCV_FLANN
 #define HAVE_OPENCV_IMGCODECS
 #define HAVE_OPENCV_IMGPROC
 #define HAVE_OPENCV_ML
